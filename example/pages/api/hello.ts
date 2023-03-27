c4c0a3bf9f7e77117bb6eb2bcd8cdc4405eea1c1
Tim Raderschad <tim.raderschad@gmail.com>
2023-03-27 07:44:04 +0200

    bump nextjs peer

--- example/pages/api/hello.ts
+++ example/pages/api/hello.ts
@@ -1,10 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { withFileUpload, getConfig, FormNextApiRequest } from '../../../dist';
+import { NextApiResponse } from 'next';
+import { withFileUpload, getConfig, FormNextApiRequest } from 'next-multiparty';
 
- async function handler(req: FormNextApiRequest, res: NextApiResponse) {
+async function handler(req: FormNextApiRequest, res: NextApiResponse) {
   res.json({ file: req.file, fields: req.fields });
-};
+}
 
-export default withFileUpload(handler)
+export default withFileUpload(handler);
 
 export const config = getConfig();
